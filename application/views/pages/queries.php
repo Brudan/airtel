164427fbe501dc7110b604e3ece01c2b73d3aaed
Enos Wakoko <enoswakoko@gmail.com>
2013-06-22 10:18:13 +0800

    Added code to pull queries from the db and tabulate them on the display. Also added a new table in the db

--- application/views/pages/queries.php
+++ application/views/pages/queries.php
@@ -8,4 +8,14 @@
  */
 ?>
 
-We'll have a list of all the queries here
+<table>
+    <tr><th>Phone Number</th><th>Category</th><th>Details</th><th>Solved</th></tr>
+<?php
+    foreach($results as $query){
+        $cat = $this->airtel_model->get_data("category","details",array("id"=>$query->cat));
+        $solved = $this->airtel_model->get_data("status",array("short_desc", "details"),array("id"=>$query->solved));
+?>
+        <tr><td><?=$query->phone?></td><td><?=$cat[0]->details?></td><td><?=$query->details?></td><td title="<?=$solved[0]->details?>"><?=$solved[0]->short_desc?></td></tr>
+<?php } ?>
+</table>
+<?php //var_dump($results); ?>
