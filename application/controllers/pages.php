09ff9bc334413077ad09f8bf2a37f18de25e2571
Enos Wakoko <enoswakoko@gmail.com>
2013-06-22 10:18:13 +0800

    Added code to pull queries from the db and tabulate them on the display. Also added a new table in the db

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -6,13 +6,13 @@
  * Time: 15:37
  * To change this template use File | Settings | File Templates.
  */
-
+$per_page = 15;
 class pages extends CI_Controller {
 
     public function __construct()
     {
         parent::__construct();
-//        $this->load->model('joze_model');
+        $this->load->model('airtel_model');
         $this->load->library('table');
         $this->load->helper('html');
         $this->load->helper('url');
@@ -33,9 +33,19 @@ class pages extends CI_Controller {
 
     public function queries()
     {
+        global $per_page;
+        $config = array();
+        $config["base_url"] = site_url('pages/queries');
+        $config["total_rows"] = $this->airtel_model->record_count("query");
+        $config["per_page"] = $per_page;
+        $config["uri_segment"] = 3;
+        $this->pagination->initialize($config);
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+        $data["results"] = $this->airtel_model->get_data("query",'','',$config["per_page"], $page);
+        $data["links"] = $this->pagination->create_links();
         $data['title'] = "Queries";
         $this->load->view('templates/header', $data);
-        $this->load->view('pages/queries');
+        $this->load->view('pages/queries', $data);
         $this->load->view('templates/footer');
     }
 
